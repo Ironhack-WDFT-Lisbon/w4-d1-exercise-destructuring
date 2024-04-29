2ec5c8e04178e6a158349959849bf2356d6ed87b
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-04-29 04:29:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# w4-d1-exercise-destructuring
+
+Considering the provided array of movies:
+
+1. Deconstruct the first 3 movies from the IMDBTop25 array and spread the rest of the movies into a new array called restOfMovies
+2. Deconstruct the score of the first movie into a new variable called firstMovieScore
+3. Deconstruct the rottenTomatoes score of the second movie from the movies array
+4. Descontruct form each movie the title and year, and store them in a new array called moviesInfo
---
+++ script.js
@@ -0,0 +1,82 @@
+const movies = [
+  {
+    title: "The Shawshank Redemption",
+    score: {
+      imdb: 9.3,
+      rottenTomatoes: 91,
+    },
+    year: 1994,
+  },
+  {
+    title: "The Godfather",
+    score: {
+      imdb: 9.2,
+      rottenTomatoes: 98,
+    },
+    year: 1972,
+  },
+  {
+    title: "The Dark Knight",
+    score: {
+      imdb: 9.0,
+      rottenTomatoes: 94,
+    },
+    year: 2008,
+  },
+  {
+    title: "The Godfather: Part II",
+    score: {
+      imdb: 9.0,
+      rottenTomatoes: 97,
+    },
+    year: 1974,
+  },
+  {
+    title: "12 Angry Men",
+    score: {
+      imdb: 8.9,
+      rottenTomatoes: 100,
+    },
+    year: 1957,
+  },
+  {
+    title: "Schindler's List",
+    score: {
+      imdb: 8.9,
+      rottenTomatoes: 97,
+    },
+    year: 1993,
+  },
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    score: {
+      imdb: 8.9,
+      rottenTomatoes: 93,
+    },
+    year: 2003,
+  },
+  {
+    title: "Pulp Fiction",
+    score: {
+      imdb: 8.9,
+      rottenTomatoes: 92,
+    },
+    year: 1994,
+  },
+  {
+    title: "The Good, the Bad and the Ugly",
+    score: {
+      imdb: 8.8,
+      rottenTomatoes: 97,
+    },
+    year: 1966,
+  },
+  {
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    score: {
+      imdb: 8.8,
+      rottenTomatoes: 91,
+    },
+    year: 2001,
+  },
+];
